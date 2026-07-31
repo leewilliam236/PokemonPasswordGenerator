7b52392c5f755aad1bfb3131243d8d5486a3f7ed
leewilliam236 <leewilliam236@gmail.com>
2026-07-30 22:48:23 -0700

    Export to excel file instead of HTML via pandas, using dict.fromkeys to remove duplicate values, and reset index via pandas

--- PokemonPasswordGenerator.py
+++ PokemonPasswordGenerator.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import pandas as pd, requests
 
+# NOTE: If you are using a VPN, turn it off, as it may block the request to the website.
+
 try:
     r = requests.get("https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_National_Pokédex_number")
 
@@ -11,14 +13,20 @@ try:
     file.close()
 
     # # Get the pokemon names from the HTML file and export into an html file.
-    file = open("pkmn.html", "w", encoding="utf-8")
+    pokedexList = []
+
     for link in soup.find_all('a'):
         if link.get('title') is not None and '(Pokémon)' in link.get('title') and link.parent.name == 'td':
-            file.write(str(link.get('title')).rstrip('(Pokémon)') + '\n')
-    file.close()
+            pokedexList.append(str(link.get('title')).rstrip('(Pokémon)'))
 
-    # Use HTML file to then generate a list.
-    pokedex = pd.read_html("pkmn.html")
+    # Removes duplicates from the list.
+    pokedexList = list(dict.fromkeys(pokedexList))
+
+    pokedex = pd.DataFrame(pokedexList, columns=['Pokémon'])
+    pokedex.index += 1  # Start index at 1 instead of 0
+
+    # Use HTML file to then generate a pd.Series object.
+    pokedex.to_excel("pokedex.xlsx", index=False)
 
 except Exception as e:
     print(f"Error occurred while writing to file: {e}")
